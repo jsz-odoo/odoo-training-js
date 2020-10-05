f59f659a2a01846daf1ef3d4a8ed6de627d027ff
Johnny Sanchez <jsz@odoo.com>
2020-10-05 23:36:05 +0000

    [ADD] Basic structure for Library Management app

---
+++ library-management/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Library Management',
+    
+    'version': '1.0',
+    
+    'description': """
+        Library Management App
+        Manage your library with ease!
+        - Organize Books
+        - Check on Rentals
+        - Manage customers and anything they have checked out
+    """,
+    
+    'author': 'Johnny Sanchez',
+    
+    'depends': ['base'],
+    
+    'data': [],
+    
+    'demo': []
+}
