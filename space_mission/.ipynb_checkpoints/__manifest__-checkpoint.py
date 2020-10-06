9055b5ab4b97daf4f9c3094ef6cdcf8ae262c1eb
Johnny Sanchez <jsz@odoo.com>
2020-10-06 20:59:26 +0000

    [ADD] Added Space Mission module

---
+++ space_mission/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Space Mission',
+    'summary': 'App to track spaceship metrics',
+    'description': """
+        Manage metrics associated with your space mission:
+        - Astronauts
+        - Spaceships
+    """,
+    'author': 'Johnny',
+    'website': 'https://www.odoo.com',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': []
+}
---
+++ space_mission/models/spaceship.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Spaceship(models.Model):
+    
+    _name = 'spacemission.spaceship'
+    _description = 'Spaceship'
+    
+    name = fields.Char(string='Spaceship', required=True)
+    description = fields.String(string='A Spaceship')
+    
+    dimensions = field.Char(string='dimensions', default='40 x 20')
+    fuel_type = field.Char(string='Fuel Type')
+    number_of_passengers = fields.Integer(string='Number of Passengers', default=10)
+    ship_type = fields.Selection(string='Ship Type',
+                                 selection=[('shuttle', 'Shuttle'),
+                                            ('freighter', 'Freighter'),
+                                            ('fighter', 'Fighter'),
+                                            ('medical', 'Medical')],
+                                 copy=False)
+    active = fields.Boolean(string='Active', default=True)
